b3e16eb732e181019701d13643dd5e98eeaef044
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-22 11:30:51 +0530

    created front screen

---
+++ Screens/FrontScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const FrontScreen = () => {
+  return (
+    <View>
+      <Text>front screen!!!</Text>
+    </View>
+  );
+};
+
+export default FrontScreen;
