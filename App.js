21889f4606eb9ee330815dd6207151ee3d39acb3
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-23 22:43:08 +0530

    implemented Stack navigation

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import COLORS from "./consts/colors";
+
+// screens
+import FrontScreen from "./Screens/FrontScreen";
+import DetailsScreen from "./Screens/DetailsScreen";
+
+// bottom navigator
+import BottomNavigator from "./Navigation/BottomNavigator";
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="FrontScreen" component={FrontScreen} />
+        <Stack.Screen name="Home" component={BottomNavigator} />
+        {/* <Stack.Screen name="DetailsScreen" component={DetailsScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
