b203ace05ddaef4a15d25a5fc02a6ecd884f2450
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-24 16:39:10 +0530

    added header Name Section and styles

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,12 +1,47 @@
 import React from "react";
-import { View, Text } from "react-native";
-
+import { View, Text, SafeAreaView, StyleSheet, Image } from "react-native";
+import COLOR from "../consts/colors";
 const Home = () => {
   return (
-    <View>
-      <Text>Home screen!!!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={{ fontSize: 28 }}>Hello,</Text>
+            <Text style={{ fontSize: 28, fontWeight: "bold" }}> Jestor</Text>
+          </View>
+          <Text style={styles.subTitle}>What do you want Today</Text>
+        </View>
+        <Image
+          source={require("../assets/images/person.png")}
+          style={styles.image}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLOR.white,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  subTitle: {
+    color: COLOR.grey,
+    fontSize: 22,
+    marginTop: 5,
+  },
+  image: {
+    height: 60,
+    width: 60,
+    resizeMode: "contain",
+    borderRadius: 50,
+  },
+});
