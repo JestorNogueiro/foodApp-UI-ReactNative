0da398e67adb1900780b47313bd147554cd3bfa3
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-24 14:52:41 +0530

    added Icons and custom style for search

--- Navigation/BottomNavigator.js
+++ Navigation/BottomNavigator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
+import { View } from "react-native";
 import Home from "../Screens/Home";
 import Cart from "../Screens/Cart";
 import COLORS from "../consts/colors";
@@ -11,29 +12,77 @@ const BottomNavigator = () => {
 
   return (
     <Tab.Navigator
-      tabBarOptions={{
+      screenOptions={{
         Style: {
           height: 55,
           borderTopWidth: 0,
           elevation: 0,
         },
-        showLabel: false,
-        activeTintColor: COLORS.primary,
+
+        activeTintColor: "#F9813A",
+        showLabel: "false",
       }}
     >
       <Tab.Screen
-        name="HomeScreen"
+        name="Home"
         component={Home}
-        option={{
+        options={{
           tabBarIcon: ({ color }) => (
             <Icon name="home-filled" color={color} size={28} />
           ),
         }}
       />
-      <Tab.Screen name="LocalMall" component={Home} />
-      <Tab.Screen name="Search" component={Home} />
-      <Tab.Screen name="Favorite" component={Home} />
-      <Tab.Screen name="Cart" component={Cart} />
+      <Tab.Screen
+        name="LocalMall"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="local-mall" color={color} size={28} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <View
+              style={{
+                height: 60,
+                width: 60,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: COLORS.white,
+                borderColor: COLORS.primary,
+                borderWidth: 2,
+                borderRadius: 30,
+                top: -20,
+                elevation: 5,
+              }}
+            >
+              <Icon name="search" color={COLORS.primary} size={28} />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="favorite" color={color} size={28} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="shopping-cart" color={color} size={28} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
