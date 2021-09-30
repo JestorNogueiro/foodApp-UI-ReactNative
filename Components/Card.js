d4b360d1730093f1afebdc4069017232e22677dd
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-30 17:18:45 +0530

    added Image, title, price for card and styles

--- Components/Card.js
+++ Components/Card.js
@@ -1,14 +1,67 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
+
+import COLORS from "../consts/colors";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+const { width } = Dimensions.get("screen");
+const cardWidth = width / 2 - 30;
 
 const Card = ({ food }) => {
   return (
-    <View>
-      <Text>{food.name}</Text>
+    <View style={styles.card}>
+      <Image source={food.image} style={styles.cardImage} />
+      <View style={{ marginHorizontal: 20 }}>
+        <Text style={{ fontSize: 18, fontWeight: "bold" }}>{food.name}</Text>
+        <Text style={{ fontSize: 18, color: COLORS.grey, marginTop: 2 }}>
+          {food.ingredients}
+        </Text>
+      </View>
+      <View style={styles.priceSection}>
+        <Text style={{ fontWeight: "bold", fontSize: 18 }}>₹ {food.price}</Text>
+        <View
+          style={{
+            backgroundColor: COLORS.primary,
+            height: 30,
+            width: 30,
+            borderRadius: 50,
+          }}
+        >
+          <Icon name="plus" />
+        </View>
+      </View>
     </View>
   );
 };
 
 export default Card;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    height: 220,
+    width: cardWidth,
+    marginTop: 50,
+    marginBottom: 20,
+    marginHorizontal: 10,
+    borderRadius: 15,
+    elevation: 13,
+    backgroundColor: COLORS.white,
+
+    // shadowColor: COLORS.dark,
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 15,
+  },
+  cardImage: {
+    width: 100,
+    height: 100,
+    top: -35,
+    alignSelf: "center",
+  },
+  priceSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+    marginTop: 8,
+  },
+});
