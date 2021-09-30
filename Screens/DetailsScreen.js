a5504bae45bd0fd8a05bae442dac04b56bd8d412
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-30 23:40:09 +0530

    adjusted header section

--- Screens/DetailsScreen.js
+++ Screens/DetailsScreen.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
 import COLORS from "../consts/colors";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
-const DetailsScreen = () => {
+const DetailsScreen = ({ navigation, route }) => {
+  const item = route.param;
+  console.log(item);
+
   return (
     <SafeAreaView style={{ backgroundColor: COLORS.white }}>
       <View style={styles.header}>
-        <Icon name="arrow-back-ios" size={28} />
+        <Icon name="arrow-back" size={28} onPress={() => navigation.goBack} />
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Details</Text>
       </View>
     </SafeAreaView>
   );
@@ -17,5 +21,10 @@ const DetailsScreen = () => {
 export default DetailsScreen;
 
 const styles = StyleSheet.create({
-  header: {},
+  header: {
+    backgroundColor: COLORS.white,
+    height: 100,
+    // justifyContent: "center",
+    flexDirection: "row",
+  },
 });
