b159965ad42b365b0fa656912fbc25ebe99ad86c
JestorNogueiro <jestornogueiro789@gmail.com>
2021-10-02 23:20:52 +0530

    added header section

--- Screens/Cart.js
+++ Screens/Cart.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, SafeAreaView, Text, View, Button } from "react-native";
 
-const Cart = () => {
+import COLORS from "../consts/colors";
+import Icon from "react-native-vector-icons/MaterialIcons";
+const Cart = ({ navigation }) => {
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Icon name="arrow-back-ios" size={28} />
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Cart</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Cart;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.white,
+  },
+  header: {
+    paddingVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+  },
+});
