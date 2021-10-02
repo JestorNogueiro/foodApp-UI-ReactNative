f52ed70470e13364e0775766e43d4a6aca3530f5
JestorNogueiro <jestornogueiro789@gmail.com>
2021-10-02 23:31:10 +0530

    added FalteList to render the cart items

--- Screens/Cart.js
+++ Screens/Cart.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text, View, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Text, View, FlatList } from "react-native";
 
 import COLORS from "../consts/colors";
 import Icon from "react-native-vector-icons/MaterialIcons";
+
+import foodData from "../consts/foodData";
+import CartCard from "../Components/CartCard";
 const Cart = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
@@ -10,6 +13,12 @@ const Cart = ({ navigation }) => {
         <Icon name="arrow-back-ios" size={28} />
         <Text style={{ fontSize: 20, fontWeight: "bold" }}>Cart</Text>
       </View>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 80 }}
+        data={foodData}
+        renderItem={({ item }) => <CartCard food={item} />}
+      />
     </SafeAreaView>
   );
 };
