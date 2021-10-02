b2ca2dc4658644878a60645b67be27f6bd506f26
JestorNogueiro <jestornogueiro789@gmail.com>
2021-10-02 23:42:30 +0530

    added image and styles

--- Components/CartCard.js
+++ Components/CartCard.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import COLORS from "../consts/colors";
 
 const CartCard = ({ food }) => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={food.image} style={styles.image} />
       <Text>{food.name}</Text>
     </View>
   );
@@ -11,4 +14,18 @@ const CartCard = ({ food }) => {
 
 export default CartCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    elevation: 15,
+    borderRadius: 10,
+    borderColor: COLORS.white,
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  image: {
+    height: 80,
+    width: 80,
+    resizeMode: "contain",
+  },
+});
